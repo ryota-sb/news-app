879719c376436ab02771d3dc62a524a78c42d5a9
ryota-sb <ryota.sb.1206@gmail.com>
2022-07-07 00:29:19 +0900

    nav sidebar component 作成

---
+++ src/components/nav/index.tsx
@@ -0,0 +1,62 @@
+import Link from "next/link";
+import Image from "next/image";
+
+const TOPICS = [
+  {
+    icon: "01",
+    path: "/",
+    title: "Top stories",
+  },
+  {
+    icon: "03",
+    path: "/topics/business",
+    title: "Business",
+  },
+  {
+    icon: "04",
+    path: "/topics/technology",
+    title: "Technology",
+  },
+  {
+    icon: "05",
+    path: "/topics/entertainment",
+    title: "Entertainment",
+  },
+  {
+    icon: "06",
+    path: "/topics/sports",
+    title: "Sports",
+  },
+];
+
+const Nav: React.FC = () => {
+  return (
+    <section>
+      <ul>
+        {TOPICS.map((topic, index) => {
+          return (
+            <li key={index}>
+              <Link href={`${topic.path}`}>
+                <a>
+                  <span>
+                    <Image
+                      src={`/img/navIcon/${topic.icon}.png`}
+                      alt={`${topic.title} icon`}
+                      loading="eager"
+                      width={33}
+                      height={33}
+                      priority
+                    />
+                  </span>
+                  <span>{topic.title}</span>
+                </a>
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </section>
+  );
+};
+
+export default Nav;
