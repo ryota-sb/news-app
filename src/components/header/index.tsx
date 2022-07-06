1fa55be73f0bc93f32132746a70e975cf9e1f17f
ryota-sb <ryota.sb.1206@gmail.com>
2022-07-06 15:33:08 +0900

    header component 修正

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,21 +1,17 @@
-import styles from "./index.module.scss";
-import Image from "next/image";
 import Link from "next/link";
 
 function Header() {
   return (
-    <section className={styles.container}>
-      <header className={styles.header}>
-        <h1 style={{ letterSpacing: "1px", textAlign: "left" }}>
-          <Link href="/">
-            <a>
-              <span style={{ fontWeight: 250 }}>Simple</span>
-              <span style={{ fontWeight: 100 }}>News</span>
-            </a>
-          </Link>
-        </h1>
-      </header>
-    </section>
+    <header className="bg-gray-400">
+      <h1>
+        <Link href="/">
+          <a>
+            <span className="text-6xl">News</span>
+            <span className="text-6xl">App</span>
+          </a>
+        </Link>
+      </h1>
+    </header>
   );
 }
 
